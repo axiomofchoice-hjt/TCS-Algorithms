cfbe83d6a71608ed3bbd121c596e4e1b14d0eccf
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-06-06 20:04:47 +0800

    fix namespace name in quicksort example

--- examples/example_inplace_stable_quicksort.cpp
+++ examples/example_inplace_stable_quicksort.cpp
@@ -6,7 +6,7 @@
 #include "tcs/inplace_stable_quicksort.hpp"
 
 int main() {
-    std::println("=== tcs::inplace_stable_qsort::inplace_stable_quicksort ===");
+    std::println("=== tcs::inplace_stable_quicksort::inplace_stable_quicksort ===");
 
     constexpr auto kShuffledOneToTen = std::array{2, 5, 9, 6, 3, 10, 1, 7, 4, 8};
     auto arr = std::vector<int64_t>(kShuffledOneToTen.begin(), kShuffledOneToTen.end());
