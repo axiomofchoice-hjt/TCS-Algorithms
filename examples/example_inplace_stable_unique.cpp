4c1d474d62442d10c35c6f537474a8f05e387b32
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-06-22 22:15:03 +0800

    cleanup unique example: use named constant for test data

--- examples/example_inplace_stable_unique.cpp
+++ examples/example_inplace_stable_unique.cpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <print>
 #include <vector>
 
@@ -8,11 +7,12 @@
 int main() {
     std::println("=== tcs::inplace_stable_unique::inplace_stable_unique ===");
 
-    std::vector<int64_t> arr = {1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7};
+    constexpr auto kArrayWithDuplicates = std::array{1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7};
+    auto arr = std::vector<int64_t>(kArrayWithDuplicates.begin(), kArrayWithDuplicates.end());
 
     print_arr(arr, "before");
 
-    auto end = tcs::inplace_stable_unique::inplace_stable_unique(arr.begin(), arr.end());
+    tcs::inplace_stable_unique::inplace_stable_unique(arr.begin(), arr.end());
 
     print_arr(arr, "after ");
 }
